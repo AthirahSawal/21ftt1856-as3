0af945129ab4d8761400e14ff205afba19af524c
Athirah Sawal <21FTT1856@student.pb.edu.bn>
2023-04-10 10:43:40 +0800

    Edit images in App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,9 +9,9 @@ import HelloWorld from './components/HelloWorld.vue'
       <img class="h-48 w-full object-cover md:h-full md:w-48" src="pictures.jpg" alt="Modern building architecture">
     </div>
     <div class="p-8">
-      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Company retreats</div>
-      <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Incredible accommodation for your team</a>
-      <p class="mt-2 text-slate-500">Looking to take your team away on a retreat to enjoy awesome food and take in some sunshine? We have a list of places to do just that.</p>
+      <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Nurathirah binti Sawal</div>
+      <a class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Level 5 Diploma in Web Development</a>
+      
     </div>
   </div>
 </div>
