031a91220acc2fb15bbda06b40387224bdca8957
Athirah Sawal <21FTT1856@student.pb.edu.bn>
2023-04-11 09:40:16 +0800

    Edit Images in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,8 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+
+  <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl bg-lime-100">
   <div class="md:flex">
     <div class="md:shrink-0">
       <img class="h-48 w-full object-cover md:h-full md:w-48" src="pictures.jpg" alt="Modern building architecture">
